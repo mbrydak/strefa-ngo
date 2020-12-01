d316b90c1cb46722ea4a062c9e737f4524eba825
Konrad Szwarc <konrad.szwarc.dev@gmail.com>
2020-12-01 22:55:52 +0100

    Add storybook addon for accessibility checking (#20)

--- development/storybook/.storybook/main.js
+++ development/storybook/.storybook/main.js
@@ -8,7 +8,12 @@ const addModulesToTranspile = (config, absolutePath) => {
 
 module.exports = {
   stories: ['../src/stories/*.stories.mdx', '../src/stories/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-a11y',
+  ],
   webpackFinal: async (config) => {
     addModulesToTranspile(config, 'packages/ui-kit');
 
